ec3950b62010894371fb574dbf6f2bb791205388
mjcox244 <mjc24cool@gmail.com>
2023-09-04 22:49:15 +0100

    Add Culling mask support Add Toggle Support Moved Quest support into a debug toggle that is enabled automaticly Moved Debug config to main config file Document more of the code for ease of understanding

--- BanterCamera/Properties/AssemblyInfo.cs
+++ BanterCamera/Properties/AssemblyInfo.cs
@@ -39,5 +39,5 @@ using MelonLoader;
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 
-[assembly: MelonInfo(typeof(CameraMod), "Banter Camera", "0.0.1", "mjcox24")]
+[assembly: MelonInfo(typeof(CameraMod), "Banter Camera", "0.1", "mjcox24")]
 [assembly: MelonGame("SideQuest", "Banter")]
